368656844774507db332a734f88517b9571bc14f
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-11-27 11:38:03 +0530

    Fix formatting issues in static data members.cpp

---
+++ static data members.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class car{
+public:
+static int wheels:
+
+};
+int car::wheels = 4;
+
+int main(){
+
+cout<<"wheels are :"<<car::wheels<<endl;
+
+  return 0;
+}
